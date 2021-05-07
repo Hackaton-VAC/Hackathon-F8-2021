cc67d98006c862ef34efa428c5e3dd2f23da5c94
Arturo Toro <arturoantoniotr@gmail.com>
2021-05-07 15:59:42 -0400

    Adding effects at buttons

--- Assets/Scripts/MyButton.cs
+++ Assets/Scripts/MyButton.cs
@@ -5,19 +5,34 @@ using UnityEngine;
 using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MyButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
-
+    public GameObject spinner;
+    public GameObject icon;
+    public GameObject pulse;
+    public GameObject bg;
+    Image image;
     public bool buttonPressed;
+
+    void Start()
+    {
+        image = bg.gameObject.GetComponent<Image>();
+    }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         buttonPressed = true;
+        pulse.SetActive(false);
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         buttonPressed = false;
+        spinner.SetActive(true);
+        icon.SetActive(false);
+        image.color = Color.black;
+        // Color Background
     }
 }
